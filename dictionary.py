506ab6e716214c150a13da047dd51ca0a5815b24
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-14 10:51:52 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+#dictionary
+user = {
+   "name":"prarthana",
+   "age":19
+}
+user["name"]
+print(user)
+user["age"]=55
+print(user)
+user["college"]="siet"
+print(user)
+del user["name"]
+print(user){'name': 'prarthana', 'age': 19}
+{'name': 'prarthana', 'age': 55}
+{'name': 'prarthana', 'age': 55, 'college': 'siet'}
+{'age': 55, 'college': 'siet'}
+age
+college
