e9bcab3418c214fc92ca6507c647c97ec0852365
parth20066 <prarthanapkhasnis@gmail.com>
2025-11-14 09:07:45 +0530

    Create moduls.py

---
+++ moduls.py
@@ -0,0 +1,26 @@
+a=[1,2,3,4,5,6]
+print(a[5])#acessing the item
+print([-3])#reverse the item
+a[3]=6#modifying the item
+print(a)
+a.append(60)#addint the items last of the list
+print(a)
+a.insert(4,77)#inserting the items inbetween
+print(a)
+a=[1,2,3,4,5,6]
+print(a[5])#acessing the item
+print([-3])#reverse the item
+a[3]=6#modifying the item
+print(a)
+a.append(60)#addint the items last of the list
+print(a)
+a.insert(4,77)#inserting the items inbetween
+print(a)
+6
+[-3]
+[1, 2, 3, 6, 5, 6]
+[1, 2, 3, 6, 5, 6, 60]
+[1, 2, 3, 6, 77, 5, 6, 60]
+
+
+
